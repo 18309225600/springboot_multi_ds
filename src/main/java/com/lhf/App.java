a14ef7b5d58e4f6673f64dd6e06f8385145990f5
liuhongfei <liuhongfei@imzhiliao.com>
2019-01-29 15:58:06 +0800

    init

---
+++ src/main/java/com/lhf/App.java
@@ -0,0 +1,17 @@
+package com.lhf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author lhf
+ * @Description
+ * @Date 2019/1/29 15:39
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class App {
+    public static void main(String[] args){
+        SpringApplication.run(App.class,args);
+    }
+}
